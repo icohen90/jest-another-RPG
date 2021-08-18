e776f0def2f24c6e97ecabd8fa58859ec1e1b034
Ian Cohen <ian_cohen90@live.com>
2021-08-18 16:27:42 -0400

    added something new

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -32,3 +32,8 @@ test('gets inventory from player or returns false', () => {
 
   expect(player.getInventory()).toEqual(false);
 });
+
+test("gets player's health value", () => {
+  const player = new Player("Dave");
+  expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+})
